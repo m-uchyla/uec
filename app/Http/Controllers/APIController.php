d4f60b621aade85a58636eca8151fa0b16356296
VEEX <czarnytroll@gmail.com>
2020-07-29 12:18:04 +0200

    response

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -14,9 +14,8 @@ class APIController extends Controller {
     public function getList(){
 
         $featured = DB::table('articles')->where('isFeatured',1)->latest()->get();
-        $data = json_decode(file_get_contents('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations'), true);
         // $obj = json_decode($body);
-        return response(['data' => $data, 'featured' => $featured])
+        return response(['data' => json_decode(file_get_contents('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations'), true), 'featured' => $featured])
             ->header('X-Api-Key','QxqirJ6zBGM45sI4xZo1X5X9_XTB4Q_54P1TyixXl2U')
             ->header('Authorization','organizer:registration')
             ->header('Range','0-49');
