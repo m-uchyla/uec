15b26cea837bd42f6723e2f8f198ae62e4d8915c
VEEX <czarnytroll@gmail.com>
2020-07-29 13:25:49 +0200

    Auth2

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -18,10 +18,10 @@ class APIController extends Controller {
             'Authorization' => '
             https://account.toornament.com/oauth2/authorize?
             response_type=code&
-            client_id={QxqirJ6zBGM45sI4xZo1X5X9_XTB4Q_54P1TyixXl2U}&
-            redirect_uri={https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations}&
-            scope={organizer:registration}&
-            state={csrf_token()}',
+            client_id=QxqirJ6zBGM45sI4xZo1X5X9_XTB4Q_54P1TyixXl2U&
+            redirect_uri=https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations&
+            scope=organizer:registration&
+            state=csrf_token()',
             'Range' => '0-49'
         ])->get('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations', [
             'name' => 'Taylor',
