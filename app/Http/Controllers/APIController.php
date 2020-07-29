64f90e20406967e0e9fe3f4c57de8a3628a39c0d
VEEX <czarnytroll@gmail.com>
2020-07-29 12:15:25 +0200

    getBody

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -14,10 +14,9 @@ class APIController extends Controller {
     public function getList(){
 
         $featured = DB::table('articles')->where('isFeatured',1)->latest()->get();
-        $Client = new Client();
-        $body = $client->get('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations')->getBody();
-        $obj = json_decode($body);
-        return response(['obj' => $obj, 'featured' => $featured])
+        $data = json_decode(file_get_contents('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations'), true);
+        // $obj = json_decode($body);
+        return response(['data' => $data, 'featured' => $featured])
             ->header('X-Api-Key','QxqirJ6zBGM45sI4xZo1X5X9_XTB4Q_54P1TyixXl2U')
             ->header('Authorization','organizer:registration')
             ->header('Range','0-49');
