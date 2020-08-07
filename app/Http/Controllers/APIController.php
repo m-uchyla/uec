f3328084ecb92add857260224592ae440d28fc02
VEEX <czarnytroll@gmail.com>
2020-08-07 20:00:25 +0200

    json again

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -19,11 +19,11 @@ class APIController extends Controller {
             'client_id' => 'e61fd46a3437441ae2ed72085mqhuwuo8gsggggskgk4og8owcckkckgcskws4kkk0000ocws8',
             'client_secret' =>'1l80dg7iyxs0s8ggocwg80s4k8gwcscc0k4gog04w4gs4gw40o',
             'scope' => 'organizer:registration',
-            ]);
+            ])->json();
 
         $response = Http::withHeaders([
             'X-Api-Key' => 'QxqirJ6zBGM45sI4xZo1X5X9_XTB4Q_54P1TyixXl2U',
-            'Authorization' => json($token),
+            'Authorization' => $token,
             'Range' => '0-49'
         ])->get('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations');
         return $response;
