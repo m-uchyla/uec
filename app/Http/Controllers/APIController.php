15bc4eb4b7ea7053d9679889b2048668b5476c57
VEEX <czarnytroll@gmail.com>
2020-08-07 19:46:59 +0200

    token as variable

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -13,11 +13,17 @@ class APIController extends Controller {
     
 
     public function getList(){
+
+        $token = Http::asForm()->post('https://api.toornament.com/oauth/v2/token', [
+            'grant_type' => 'client_credentials',
+            'client_id' => 'e61fd46a3437441ae2ed72085mqhuwuo8gsggggskgk4og8owcckkckgcskws4kkk0000ocws8',
+            'client_secret' =>'1l80dg7iyxs0s8ggocwg80s4k8gwcscc0k4gog04w4gs4gw40o',
+            'scope' => 'organizer:registration',
+            ]);
+
         $response = Http::withHeaders([
             'X-Api-Key' => 'QxqirJ6zBGM45sI4xZo1X5X9_XTB4Q_54P1TyixXl2U',
-            'Authorization' => Http::withBody(
-                'grant_type=client_credentials&client_id=s6BhdRkqt3&client_secret=7Fjfp0ZBr1KtDRbnfVdmIw'
-            )->post('https://api.toornament.com/oauth/v2/token'),
+            'Authorization' => $token,
             'Range' => '0-49'
         ])->get('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations');
         return $response;
