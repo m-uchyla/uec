9e52c2c0ba8581cf301f58961229f8a78258d9b5
VEEX <czarnytroll@gmail.com>
2020-08-21 11:23:10 +0200

    trumbowyg editor

--- resources/views/newarticle.blade.php
+++ resources/views/newarticle.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.admin')
 
+@section('style')
+<link rel="stylesheet" href="trumbowyg/dist/ui/trumbowyg.min.css">
+@endsection
+
 @section('content')
 
 <div class="clearfix"></div>
@@ -51,8 +55,14 @@
 @endsection
 
 @section('script')
+<script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+<script src="trumbowyg/dist/trumbowyg.min.js"></script>
+
 
 <script>
+
+$('#content').trumbowyg();
 
 function charcountupdate(str) {
 	var lng = str.length;
