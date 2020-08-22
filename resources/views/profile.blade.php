45cf22cbccbcd703dfc8e6c88c7b05def55102e3
VEEX <czarnytroll@gmail.com>
2020-08-22 13:01:48 +0200

    form for profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -13,31 +13,43 @@
            @csrf
            <div class="form-group">
             <label for="input-1" id="title">Imię</label>
-            <input type="text" class="form-control" id="name" placeholder="{{$user->name}}" maxlength="50" name="name" autofocus required>
+            <input type="text" class="form-control" id="name" value="{{$user->name}}" maxlength="50" name="name" autofocus required>
            </div>
            <div class="form-group">
             <label for="input-1" id="title">Nick</label>
-            <input type="text" class="form-control" id="nick" placeholder="{{$user->nick}}" maxlength="50" name="nick"  required>
+            <input type="text" class="form-control" id="nick" value="{{$user->nick}}" maxlength="50" name="nick"  required>
            </div>
            <div class="form-group">
             <label for="input-1" id="title">Nazwisko</label>
-            <input type="text" class="form-control" id="lastName" placeholder="{{$user->lastName}}" maxlength="50" name="lastName"  required>
+            <input type="text" class="form-control" id="lastName" value="{{$user->lastName}}" maxlength="50" name="lastName"  required>
            </div>
            <div class="form-group">
             <label for="input-1" id="title">E-mail</label>
-            <input type="text" class="form-control" id="email" placeholder="{{$user->email}}" maxlength="50" name="email" required>
+            <input type="text" class="form-control" id="email" value="{{$user->email}}" maxlength="50" name="email" required>
            </div>
            <div class="form-group">
             <label for="input-1" id="title">Steam ID 64</label>
+            @if ( $user->steamID == null )
             <input type="text" class="form-control" id="steamID" placeholder="Podaj steamID 64 (np. przy użyciu www.steamid.io)" maxlength="50" name="steamID">
+            @else
+            <input type="text" class="form-control" id="steamID" value="{{$user->steamID}}" maxlength="50" name="steamID">
+            @endif
            </div>
            <div class="form-group">
             <label for="input-2">Data urodzenia</label>
+            @if ( $user->dateOfBirth == null )
             <input type="date" class="form-control" id="dateOfBirth" name="dateOfBirth">
+            @else
+            <input type="date" class="form-control" value="{{$user->dateOfBirth}}" id="dateOfBirth" name="dateOfBirth">
+            @endif
            </div>
            <div class="form-group">
             <label for="input-3">Zdjęcie</label>
+            @if ( $user->photo == null )
             <input type="text" class="form-control" id="photo" maxlength="200" placeholder="Podaj link do Twojego zdjęcia (np. przy użyciu https://imgur.com/)" name="photo">
+            @else
+            <input type="text" class="form-control" id="photo" maxlength="200" value="{{$user->photo}}" name="photo">
+            @endif
            </div>
            <div class="form-group">
             <button type="submit" class="btn btn-light px-5"><i class="zmdi zmdi-assignment"></i>  Aktualizuj</button>
