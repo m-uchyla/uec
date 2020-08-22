8d026848a302e8541de0375a54ed0d8f1c6cb5d6
VEEX <czarnytroll@gmail.com>
2020-08-22 15:00:29 +0200

    auth user

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -21,7 +21,7 @@
             </ul>
             <div class="tab-content p-3">
                 <div class="tab-pane active" id="profile">
-                    <h5 class="mb-3">{{Auth::user->name}} "{{Auth::user->nick}}" {{Auth::user->lastName}}</h5>
+                    <h5 class="mb-3">{{Auth::user()->name}} "{{Auth::user()->nick}}" {{Auth::user()->lastName}}</h5>
                     <div class="row">
                         <div class="col-md-6">
                             <h6>Kontakt</h6>
