1c2ea26f43be48ba1fe87c728a0ea8c54d00278b
VEEX <czarnytroll@gmail.com>
2020-09-01 10:53:22 +0200

    profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -26,28 +26,28 @@
            </div>
            <div class="form-group">
             <label for="input-1" id="title">E-mail</label>
-            <input type="text" class="form-control" id="email" value="{{$user->email}}" maxlength="50" name="email" required>
+            <input type="email" class="form-control" id="email" value="{{$user->email}}" maxlength="50" name="email" required>
            </div>
            <div class="form-group">
             <label for="input-1" id="title">Steam ID 64</label>
             @if ( $user->steamID == null )
-            <input type="text" class="form-control" id="steamID" placeholder="Podaj steamID 64 (np. przy użyciu www.steamid.io)" maxlength="50" name="steamID">
+            <input type="number" class="form-control" id="steamID" placeholder="Podaj steamID 64 (np. przy użyciu www.steamid.io)" minlength="17" maxlength="17" name="steamID">
             @else
-            <input type="text" class="form-control" id="steamID" value="{{$user->steamID}}" maxlength="50" name="steamID">
+            <input type="number" class="form-control" id="steamID" value="{{$user->steamID}}" minlength="17" maxlength="17" name="steamID">
             @endif
            </div>
            <div class="form-group">
             <label for="input-2">Data urodzenia</label>
             @if ( $user->dateOfBirth == null )
-            <input type="date" class="form-control" id="dateOfBirth" name="dateOfBirth">
+            <input type="date" class="form-control" id="dateOfBirth" name="dateOfBirth" max="2015-01-01" min="1915-01-01">
             @else
-            <input type="date" class="form-control" value="{{$user->dateOfBirth}}" id="dateOfBirth" name="dateOfBirth">
+            <input type="date" class="form-control" value="{{$user->dateOfBirth}}" id="dateOfBirth" name="dateOfBirth" max="2015-01-01" min="1915-01-01"> 
             @endif
            </div>
            <div class="form-group">
             <label for="input-3">Zdjęcie</label>
             @if ( $user->photo == null )
-            <input type="text" class="form-control" id="photo" maxlength="200" placeholder="Podaj link do Twojego zdjęcia (np. przy użyciu https://imgur.com/)" name="photo">
+            <input type="url" class="form-control" id="photo" maxlength="200" placeholder="Podaj link do Twojego zdjęcia (np. przy użyciu https://imgur.com/)" name="photo">
             @else
             <input type="text" class="form-control" id="photo" maxlength="200" value="{{$user->photo}}" name="photo">
             @endif
