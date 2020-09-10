5fb01d82439925e76c67155bb87d31e82d7387b7
VEEX <czarnytroll@gmail.com>
2020-09-10 13:16:49 +0200

    sign in blade

--- resources/views/uec.blade.php
+++ resources/views/uec.blade.php
@@ -5,7 +5,7 @@
 	
   <div class="content-wrapper">
     <div class="container-fluid">
-
+@if(false)
 <div class="card-body">
            <div class="card-title">Zapisz się do najbliższej edycji Underground Esport Cup CS:GO!</div>
            <hr>
@@ -32,21 +32,39 @@
             <label for="input-1" id="title">Kapitan</label>
             <input type="email" class="form-control" id="ownerID" value="ownerID" maxlength="50" name="ownerID" required>
            </div>
-           <div class="form-group">
-            <label for="input-1" id="title">Wyznacz podstawowy skład (max 5 osób)</label>
-            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 1<br>
-            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 2<br>
-            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 3<br>
-            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 4<br>
-            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 5<br>
-            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 6<br>
-            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike">Level 7<br>
+           <div class="form-group" style="margin-top: 30px; margin-bottom: 25px">
+            <label for="input-1" id="title">Wyznacz podstawowy skład (5 osób)</label><br>
+            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike"> Level 1<br>
+            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike"> Level 2<br>
+            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike"> Level 3<br>
+            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike"> Level 4<br>
+            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike"> Level 5<br>
+            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike"> Level 6<br>
+            <input class="single-checkbox" type="checkbox" name="vehicle" value="Bike"> Level 7<br>
            </div>
            <div class="form-group">
             <button type="submit" class="btn btn-light px-5"><i class="zmdi zmdi-assignment"></i>  Zapisz się!</button>
           </div>
           </form>
          </div>
+@elseif
+<div class="card-title">Jak zapisać się do najbliższej edycji Underground Esport Cup CS:GO?</div>
+<hr>
+<p> Aby Twoja drużyna mogła wziąć udział w rozgrywkach UEC wypełnij poniższe instrukcje w podanej kolejności: </p>
+<ol>
+  <li>Każdy z zawodników powinien założyć swoje osobne konto na portalu UEC</li>
+  <li>Po założeniu konta każdy gracz musi wypełnić wszystkie informacje w zakładce <a href="{{ route('profile') }}">EDYTUJ PROFIL</a></li>
+  <li>Założyciel drużyny, po uzupełnieniu informacji ma za zadanie udać się do zakładki <a href="{{ route('dashboard') }}">PANEL GŁÓWNY</a> i utworzyć drużynę</li>
+  <li>W tej samej zakładce po przejściu do zarządzania drużyną kapitan ma możliwość zaprosić pozostałych członków drużyny</li>
+  <li>Po wysłaniu zaproszeń, gracze powinni zaakceptować zaproszenia</li>
+  <li>Jeżeli wszyscy gracze należą już do drużyny, kapitan może powrócić w to miejsce, pojawi się tutaj formularz zgłoszeniowy</li>
+  <li>Po wysłaniu formularza pozostało oczekiwanie na decyzję administracji. Jeżeli drużyna oraz gracze posiadają poprawne informacje, oraz wszelkie formularze są bezbłędnie wypełnione, Twoja drużyna pojawi się wśród drużyn zakwalifikowanych do turnieju</li>
+  <li>Lista zakwalifikowanych drużyn dostępna jest w zakładce <a href="{{ route('teams') }}">DRUŻYNY</a></li>
+</ol>
+<br>
+<br>
+<div class="card-title">UWAGA: Po wysłaniu formularza Twoje dane nie mogą być zmienione. Aby zaktualizować informacje o Tobie lub Twojej drużynie, edytuj odpowiednie pola na stronie, a następnie powiadom o tym administrację turnieju!</div>
+@endif
          </div>
 	 </div>
 	</div><!--End Row-->
