0ba2cb6e079f8c7ab3cca63d1c0adbeb75c74551
VEEX <czarnytroll@gmail.com>
2020-09-14 09:36:55 +0200

    token scope

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -11,12 +11,12 @@ use Illuminate\Support\Facades\Http;
 class APIController extends Controller {
 
 
-    private function getToken(){
+    private function getToken($scope){
         return (Http::asForm()->post('https://api.toornament.com/oauth/v2/token', [
             'grant_type' => 'client_credentials',
             'client_id' => 'e61fd46a3437441ae2ed72085mqhuwuo8gsggggskgk4og8owcckkckgcskws4kkk0000ocws8',
             'client_secret' =>'1l80dg7iyxs0s8ggocwg80s4k8gwcscc0k4gog04w4gs4gw40o',
-            'scope' => 'organizer:registration',
+            'scope' => 'organizer:'.$scope,
             ])->json())['access_token'];
     }
 
@@ -24,7 +24,7 @@ class APIController extends Controller {
 
         $registrations = Http::withHeaders([
             'X-Api-Key' => 'QxqirJ6zBGM45sI4xZo1X5X9_XTB4Q_54P1TyixXl2U',
-            'Authorization' => $this->getToken(),
+            'Authorization' => $this->getToken('participant'),
             'Range' => 'participants=0-49'
         ])->get('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/participants');
 
@@ -37,7 +37,7 @@ class APIController extends Controller {
 
         $first = Http::withHeaders([
             'X-Api-Key' => 'QxqirJ6zBGM45sI4xZo1X5X9_XTB4Q_54P1TyixXl2U',
-            'Authorization' => $this->getToken(),
+            'Authorization' => $this->getToken('result'),
             'Range' => 'items=0-49'
         ])->get('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/stages/3920455650677407744/ranking-items');
         
@@ -83,7 +83,7 @@ class APIController extends Controller {
 
         $response = Http::withHeaders([
             'X-Api-Key' => 'QxqirJ6zBGM45sI4xZo1X5X9_XTB4Q_54P1TyixXl2U',
-            'Authorization' => $this->getToken(),
+            'Authorization' => $this->getToken('registration'),
         ])->post('https://api.toornament.com/organizer/v2/tournaments/3784327726246748160/registrations', [
                 "name" => $name,
                 "email" => $email,
