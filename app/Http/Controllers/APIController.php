f152d1b78eee6fc8f39e016507add2c260a10c13
VEEX <czarnytroll@gmail.com>
2020-09-14 17:35:09 +0200

    date with parameter

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -225,7 +225,7 @@ class APIController extends Controller {
             'Range' => 'matches=0-99'
         ])->get($this->toornament_link.$this->tournament_id.'/matches', [
             'is_scheduled' => 1,
-            'scheduled_after' => date()
+            'scheduled_after' => date('Y-m-d H:i:s')
         ]);
         $matches= json_decode($matches);
 
