464c056c7c18dc1a4ce1290f112b7aa6b68869ee
VEEX <czarnytroll@gmail.com>
2020-09-26 19:30:05 +0200

    paragraph

--- resources/views/uec2.blade.php
+++ resources/views/uec2.blade.php
@@ -7,6 +7,6 @@
 
 <body>
 @for ($i = 0; $i < count($list); $i++)
-<li>{{ json_encode($list[$i], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) }}</li>
+<p>{{ json_encode($list[$i], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES) }}</p>
 @endfor
 </body
