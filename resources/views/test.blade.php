ac2ab04e6d9574a1256bcd9183b264c041469386
Jakub Milewski <kubim2002@gmail.com>
2020-10-07 01:23:47 +0200

    bracket 2

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.app')
 @section('content')
-<div class="col-xl-12 col-lg-12 col-md-12 col-sm-" style="margin-bottom:30px">
+<div class="team-area section-padding30" id='bracket'>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-6 col-lg-7 col-md-10 col-sm-10">
+                         <!-- Section Tittle -->
+                         <div class="section-tittle text-center mb-70">
+                            <span>Etap Trzeci</span>
+                            <h2>Dogrywki fazy grupowej</h2>
+                        </div> 
+                    </div>
+                </div>
+                <div class="row">
+                    <!-- single Tem -->
+                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-" style="margin-bottom:30px">
                     <h2 style="text-align:center">Grupa E</h2>
                     <div class="progress-table-wrap">
 						<div class="progress-table">
@@ -13,4 +26,20 @@
 								<div class="visit">+/-</div>
 								<div class="visit">Punkty</div>
 							</div>
+						    @else
+									<div class="table-row">
+										<div class="serial"></div>
+										<div class="country"></div>
+										<div class="visit"></div>
+										<div class="visit"></div>
+										<div class="visit"></div>
+										<div class="visit"></div>
+										<div class="visit"></div>
+									</div>
+								
+							@endfor
+						</div>
+					</div>
+            </div>
+</div>                
 @endsection
