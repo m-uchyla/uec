2e5a76b73acf4fc8a0e4c3bfc599467d075d63b3
Jakub Milewski <kubim2002@gmail.com>
2020-10-07 01:11:03 +0200

    bracket 1

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.app')
 @section('content')
-<h1>Test bo nie umiem</h1>
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-" style="margin-bottom:30px">
+                    <h2 style="text-align:center">Grupa E</h2>
+                    <div class="progress-table-wrap">
+						<div class="progress-table">
+							<div class="table-head">
+								<div class="serial">#</div>
+								<div class="country">Drużyna</div>
+								<div class="visit">Zwycięstwa</div>
+								<div class="visit">Porażki</div>
+								<div class="visit">Meczy</div>
+								<div class="visit">+/-</div>
+								<div class="visit">Punkty</div>
+							</div>
 @endsection
