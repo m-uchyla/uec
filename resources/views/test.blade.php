7a7c4ea5f757ead8f98af0e3332fadd3afae6dc3
Jakub Milewski <kubim2002@gmail.com>
2020-10-11 22:30:06 +0200

    button test 2

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="f1-background">
         <!-- Title Cup -->
-        <div>
+        <div class="section-1-f1">
         <h2 class="f1"> Sezon 1 </h2>
         <br>
         <h1 class="f1"> UEC FORMULA 1 </h1>
