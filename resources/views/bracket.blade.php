501a33350e87739b272e90e71622191ebae33ffe
Jakub Milewski <kubim2002@gmail.com>
2020-10-11 02:11:04 +0200

    test teamname-a

--- resources/views/bracket.blade.php
+++ resources/views/bracket.blade.php
@@ -34,7 +34,7 @@
 								<div class="serial">{{$groups["A"][$i]->rank}}</div>
 								<div class="country">
 									@if($groups["A"][$i]->participant->custom_fields->fanpage)
-									<a href='{{$groups["A"][$i]->participant->custom_fields->fanpage}}' style='color:#b91010'>
+									<a href='{{$groups["A"][$i]->participant->custom_fields->fanpage}}' class="teamname-a">
 									{{$groups["A"][$i]->participant->name}}</a>
 									@else
 									{{$groups["A"][$i]->participant->name}}
