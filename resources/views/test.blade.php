17404287db5d0c32cc84f73e9f80a166b410f428
Jakub Milewski <kubim2002@gmail.com>
2020-10-12 06:05:37 +0200

    fix

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,33 +3,33 @@
     <div class="f1-background">
         <!-- Title Cup -->
         <div class="section-1-f1">
-            <h2 id="f1"> Sezon 1 </h2>
+            <h2 class="f1"> Sezon 1 </h2>
             <br>
-            <h1 id="f1"> UEC FORMULA 1 </h1>
+            <h1 class="f1"> UEC FORMULA 1 </h1>
                 <div>
                     <a class="button-f1">Zarejestruj się</a>
                 </div>
                 <div class="social-f1">
                     <a class="item">
-                        <div class="content-media">
+                        <div id="content-media">
                             <h2>Facebook</h3>
                             <h3>undergroundesportcup</h3>
                         </div>
                     </a>
                     <a class="item">
-                        <div class="content-media">
+                        <div id="content-media">
                             <h2>Youtube</h3>
                             <h3>undergroundesportcup</h3>
                         </div>
                     </a>   
                     <a class="item">
-                        <div class="content-media">
+                        <div id="content-media">
                             <h2>Twitch</h3>
                             <h3>mil3k00</h3>
                         </div>
                     </a>
                     <a class="item">
-                        <div class="content-media">
+                        <div id="content-media">
                             <h2>Facebook</h3>
                             <h3>qn3k</h3>
                         </div>
