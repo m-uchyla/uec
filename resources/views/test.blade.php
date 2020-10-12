c75eaa8b87a2888b2d6f387a63fc94c4eecec8f6
Jakub Milewski <kubim2002@gmail.com>
2020-10-12 02:01:20 +0200

    test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -9,5 +9,12 @@
             <div>
                 <a class="button-f1">Zarejestruj się</a>-
             </div>
+            <div class="social-f1">
+                <a class="item">
+                    <div class="content-media">
+                        <h2>Facebook<h3>
+                        <h3>undergroundesportcup<h3>
+                    <div>
+            </div>
     </div>
 @endsection
