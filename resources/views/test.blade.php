1024d84574ff63b49d9641112800436e4fc317b5
Jakub Milewski <kubim2002@gmail.com>
2020-10-12 06:15:58 +0200

    fix white

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,38 +3,40 @@
     <div class="f1-background">
         <!-- Title Cup -->
         <div class="section-1-f1">
-            <h2 class="f1"> Sezon 1 </h2>
-            <br>
-            <h1 class="f1"> UEC FORMULA 1 </h1>
-                <div>
-                    <a class="button-f1">Zarejestruj się</a>
-                </div>
-                <div class="social-f1">
-                    <a class="item">
-                        <div id="content-media">
-                            <h2>Facebook</h3>
-                            <h3>undergroundesportcup</h3>
-                        </div>
-                    </a>
-                    <a class="item">
-                        <div id="content-media">
-                            <h2>Youtube</h3>
-                            <h3>undergroundesportcup</h3>
-                        </div>
-                    </a>   
-                    <a class="item">
-                        <div id="content-media">
-                            <h2>Twitch</h3>
-                            <h3>mil3k00</h3>
-                        </div>
-                    </a>
-                    <a class="item">
-                        <div id="content-media">
-                            <h2>Facebook</h3>
-                            <h3>qn3k</h3>
-                        </div>
-                    </a>
-                </div>
+            <div class="special-backgorund">
+                <h2 class="f1"> Sezon 1 </h2>
+                <br>
+                <h1 class="f1"> UEC FORMULA 1 </h1>
+                    <div>
+                        <a class="button-f1">Zarejestruj się</a>
+                    </div>
+                    <div class="social-f1">
+                        <a class="item">
+                            <div id="content-media">
+                                <h2 id="kurwa">Facebook</h3>
+                                <h3 id="kurwa">undergroundesportcup</h3>
+                            </div>
+                        </a>
+                        <a class="item">
+                            <div id="content-media">
+                                <h2 id="kurwa">Youtube</h3>
+                                <h3 id="kurwa">undergroundesportcup</h3>
+                            </div>
+                        </a>   
+                        <a class="item">
+                            <div id="content-media">
+                                <h2 id="kurwa">Twitch</h3>
+                                <h3 id="kurwa">mil3k00</h3>
+                            </div>
+                        </a>
+                        <a class="item">
+                            <div id="content-media">
+                                <h2 id="kurwa">Facebook</h3>
+                                <h3 id="kurwa">qn3k</h3>
+                            </div>
+                        </a>
+                    </div>
+            </div>
         </div>
     </div>
 @endsection
