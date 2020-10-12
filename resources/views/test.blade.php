42310bbd24196cc1241ae68a320db077037f2ab6
Jakub Milewski <kubim2002@gmail.com>
2020-10-12 02:12:04 +0200

    test 2

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,18 +3,34 @@
     <div class="f1-background">
         <!-- Title Cup -->
         <div class="section-1-f1">
-        <h2 class="f1"> Sezon 1 </h2>
-        <br>
-        <h1 class="f1"> UEC FORMULA 1 </h1>
-            <div>
-                <a class="button-f1">Zarejestruj się</a>-
-            </div>
-            <div class="social-f1">
-                <a class="item">
-                    <div class="content-media">
-                        <h2>Facebook</h3>
-                        <h3>undergroundesportcup</h3>
-                    <div>
-            </div>
+            <h2 class="f1"> Sezon 1 </h2>
+            <br>
+            <h1 class="f1"> UEC FORMULA 1 </h1>
+                <div>
+                    <a class="button-f1">Zarejestruj się</a>-
+                </div>
+                <div class="social-f1">
+                    <a class="item">
+                        <div class="content-media">
+                            <h2>Facebook</h3>
+                            <h3>undergroundesportcup</h3>
+                        </div>
+                    <a class="item">
+                        <div class="content-media">
+                            <h2>Youtube</h3>
+                            <h3>undergroundesportcup</h3>
+                        </div>
+                    <a class="item">
+                        <div class="content-media">
+                            <h2>Twitch</h3>
+                            <h3>mil3k00</h3>
+                        </div>
+                    <a class="item">
+                        <div class="content-media">
+                            <h2>Facebook</h3>
+                            <h3>qn3k</h3>
+                        </div>
+                </div>
+        </div>
     </div>
 @endsection
