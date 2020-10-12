da6fbaf85c19b4f8c0bd506ac53db220d0d791a5
Jakub Milewski <kubim2002@gmail.com>
2020-10-12 02:55:23 +0200

    sec 1 test

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,7 +7,7 @@
             <br>
             <h1 class="f1"> UEC FORMULA 1 </h1>
                 <div>
-                    <a class="button-f1">Zarejestruj się</a>-
+                    <a class="button-f1">Zarejestruj się</a>
                 </div>
                 <div class="social-f1">
                     <a class="item">
@@ -15,21 +15,25 @@
                             <h2>Facebook</h3>
                             <h3>undergroundesportcup</h3>
                         </div>
+                    </a>
                     <a class="item">
                         <div class="content-media">
                             <h2>Youtube</h3>
                             <h3>undergroundesportcup</h3>
                         </div>
+                    </a>   
                     <a class="item">
                         <div class="content-media">
                             <h2>Twitch</h3>
                             <h3>mil3k00</h3>
                         </div>
+                    </a>
                     <a class="item">
                         <div class="content-media">
                             <h2>Facebook</h3>
                             <h3>qn3k</h3>
                         </div>
+                    </a>
                 </div>
         </div>
     </div>
