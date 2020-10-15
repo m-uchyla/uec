90a71aeaf1eda47c4f305bbbae62666b5144dd8c
VEEX <czarnytroll@gmail.com>
2020-10-15 17:39:06 +0200

    WebStats

---
+++ app/Http/Middleware/WebStats.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use DB;
+
+class WebStats
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    
+    public function handle($request, Closure $next)
+    {
+        if ($request->isMethod('get')) {
+            
+            date_default_timezone_set('GMT+2');
+            $now = date('Y-m');
+            
+            if(DB::table('stats')->where('month', $now)->exists()){
+                DB::table('stats')->where('month', $now)->increment('views');
+            }else{
+                $data=array(
+                    "month"=>$now,
+                    "views"=>1,
+                );
+                DB::table('stats')->insert($data);
+            }
+        }
+        return $next($request);
+    }
+}
