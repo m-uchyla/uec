b9874d326ea311823f7464011d32d046337ee908
Jakub Milewski <kubim2002@gmail.com>
2020-12-21 16:57:09 +0100

    test bootstrap

--- resources/views/schedule-f1.blade.php
+++ resources/views/schedule-f1.blade.php
@@ -1,5 +1,49 @@
 @extends('layouts.f1')
 @section('content')
-
-        
+<div class="container">
+  <div class="row">
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+    <div class="col-sm col-md-6 col-lg-4">
+      One of three columns
+    </div>
+  </div>
+</div>
 @endsection
