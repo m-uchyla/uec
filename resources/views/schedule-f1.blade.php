741ce1a9a273563a0de2d70d262e433c841247cf
Jakub Milewski <kubim2002@gmail.com>
2020-12-21 17:11:00 +0100

    tets

--- resources/views/schedule-f1.blade.php
+++ resources/views/schedule-f1.blade.php
@@ -1,49 +1,50 @@
 @extends('layouts.f1')
 @section('content')
-<div class="container">
-  <div class="row">
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
+<div class="f1-background">
+    <div class="container">
+    <div class="row">
+        <div class="col-sm col-md-6 col-lg-4">
+            
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
     </div>
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
+    <div class="row">
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
     </div>
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
+    <div class="row">
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
     </div>
-  </div>
-  <div class="row">
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
+    <div class="row">
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        One of three columns
+        </div>
     </div>
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
     </div>
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
-    </div>
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
-    </div>
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
-    </div>
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
-    </div>
-    <div class="col-sm col-md-6 col-lg-4">
-      One of three columns
-    </div>
-  </div>
-</div>
 @endsection
