97ecf64582cd0641ff6cbd95401c7df75c470869
Jakub Milewski <kubim2002@gmail.com>
2021-01-10 18:16:54 +0100

    dd

--- resources/views/schedule-f1.blade.php
+++ resources/views/schedule-f1.blade.php
@@ -4,46 +4,46 @@
     <div class="container">
     <div class="row">
         <div class="col-sm col-md-6 col-lg-4">
-        chuj coś tam
+        <p class="schedule-name">Bahrian</p>
         </div>
         <div class="col-sm col-md-6 col-lg-4">\
-        chuj coś tam
+        <p class="schedule-name">Wietnam</p>
         </div>
         <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
-        </div>
-        <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
-        </div>
-        <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
+        <p class="schedule-name">Azerbejdżan</p>
         </div>
     </div>
     <div class="row">
         <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
+        <p class="schedule-name">Monako</p>
         </div>
         <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
+        <p class="schedule-name">Kanada</p>
         </div>
         <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
+        <p class="schedule-name">Austria</p>
         </div>
     </div>
     <div class="row">
         <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
+        <p class="schedule-name">Wielka Brytania</p>
         </div>
         <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
+        <p class="schedule-name">Węgry</p>
         </div>
         <div class="col-sm col-md-6 col-lg-4">
-        One of three columns
+        <p class="schedule-name">Belgia</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm col-md-6 col-lg-4">
+        <p class="schedule-name">Holandia</p>
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        <p class="schedule-name">Włochy</p>
+        </div>
+        <div class="col-sm col-md-6 col-lg-4">
+        <p class="schedule-name">Brazylia</p>
         </div>
     </div>
     </div>
