a05b7226e901c3dfb39a2e5e7049d66798408fb4
Jakub Jankowski <fenus021@gmail.com>
2021-01-29 03:09:28 +0100

    8a

--- resources/views/schedule-f1.blade.php
+++ resources/views/schedule-f1.blade.php
@@ -10,10 +10,10 @@
             <div class="col-sm col-md-6 col-lg-4 schedule-tor">
                 <div class="schedule-name">Bahrian</div>
                 <div class="schedule-track-flag">
-                    <img src="{{ asset('resources/main/img/tracks/bahrain_flag.png', true) }}" alt="bahrain-flag">
+                    <img style="width:100%" src="{{ asset('resources/main/img/tracks/bahrain_flag.png', true) }}" alt="bahrain-flag">
                 </div>
                 <div class="schedule-track">
-                    <img src="{{ asset('resources/main/img/tracks/bahrain1.png', true) }}" alt="bahrain-track">
+                    <img style="width:100%" src="{{ asset('resources/main/img/tracks/bahrain1.png', true) }}" alt="bahrain-track">
                 </div>
                 <div class="schedule-track-date">3 marca 2021</div>
             </div>
