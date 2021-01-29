58eb48fb098f0a160bbcc976b3ab440235ab8ba8
Jakub Jankowski <fenus021@gmail.com>
2021-01-29 05:15:15 +0100

    14

--- resources/views/schedule-f1.blade.php
+++ resources/views/schedule-f1.blade.php
@@ -37,8 +37,8 @@
             </div>
             <div class="col-sm col-md-6 col-lg-4 schedule-tor">
             <div class="schedule-name">Wietnam</div>
-                <img class="schedule-track-flag" src="{{ asset('resources/main/img/tracks/vietnam_flag.png', true) }}" alt="bahrain-flag">
-                <img class="schedule-track" src="{{ asset('resources/main/img/tracks/vietnam1.png', true) }}" alt="bahrain-track">
+                <img class="schedule-track-flag" src="{{ asset('resources/main/img/tracks/vietnam_flag.png', true) }}" alt="vietnam-flag">
+                <img class="schedule-track" src="{{ asset('resources/main/img/tracks/vietnam1.png', true) }}" alt="vietnam-track">
                 <div class="schedule-box">
                     <div class="schedule-title">Data wyścigu</div>
                     <div class="schedule-data">7 marca 2021</div>
@@ -63,8 +63,8 @@
             </div>
             <div class="col-sm col-md-6 col-lg-4 schedule-tor">
             <div class="schedule-name">Holandia</div>
-                <img class="schedule-track-flag" src="{{ asset('resources/main/img/tracks/netherland_flag.png', true) }}" alt="bahrain-flag">
-                <img class="schedule-track" src="{{ asset('resources/main/img/tracks/netherland1.png', true) }}" alt="bahrain-track">
+                <img class="schedule-track-flag" src="{{ asset('resources/main/img/tracks/netherland_flag.png', true) }}" alt="netherland-flag">
+                <img class="schedule-track" src="{{ asset('resources/main/img/tracks/netherland1.png', true) }}" alt="netherland-track">
                 <div class="schedule-box">
                     <div class="schedule-title">Data wyścigu</div>
                     <div class="schedule-data">10 marca 2021</div>
@@ -87,31 +87,100 @@
                     <div class="schedule-data">153.324 km</div> 
                 </div>
             </div>
-            <div class="col-sm col-md-6 col-lg-4">
-                <p class="schedule-name">Monako</p>
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
+            <div class="schedule-name">Monako</div>
+                <img class="schedule-track-flag" src="{{ asset('resources/main/img/tracks/monaco_flag.png', true) }}" alt="monaco-flag">
+                <img class="schedule-track" src="{{ asset('resources/main/img/tracks/monaco1.png', true) }}" alt="monaco-track">
+                <div class="schedule-box">
+                    <div class="schedule-title">Data wyścigu</div>
+                    <div class="schedule-data">14 marca 2021</div>
+                </div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Nazwa toru</div>
+                    <div class="schedule-data">Circuit de Monaco</div>
+                </div>
+                <div class="schedule-header">Dane wyścigu</div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Długość okrążenia</div>
+                    <div class="schedule-data">3.337 km</div>
+                </div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Ilość okrążeń</div>
+                    <div class="schedule-data">39</div>
+                </div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Długość wyścigu</div>
+                    <div class="schedule-data">130.143 km</div> 
+                </div>
             </div>
-            <div class="col-sm col-md-6 col-lg-4">
-                <p class="schedule-name">Kanada</p>
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
+            <div class="schedule-name">Azerbejżan</div>
+                <img class="schedule-track-flag" src="{{ asset('resources/main/img/tracks/baku_flag.png', true) }}" alt="azerbaijan-flag">
+                <img class="schedule-track" src="{{ asset('resources/main/img/tracks/baku1.png', true) }}" alt="azerbaijan-track">
+                <div class="schedule-box">
+                    <div class="schedule-title">Data wyścigu</div>
+                    <div class="schedule-data">17 marca 2021</div>
+                </div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Nazwa toru</div>
+                    <div class="schedule-data">Baku City Circuit</div>
+                </div>
+                <div class="schedule-header">Dane wyścigu</div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Długość okrążenia</div>
+                    <div class="schedule-data">6.003 km</div>
+                </div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Ilość okrążeń</div>
+                    <div class="schedule-data">26</div>
+                </div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Długość wyścigu</div>
+                    <div class="schedule-data">156.078 km</div> 
+                </div>
             </div>
-            <div class="col-sm col-md-6 col-lg-4">
-                <p class="schedule-name">Austria</p>
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
+            <div class="schedule-name">Kanada</div>
+                <img class="schedule-track-flag" src="{{ asset('resources/main/img/tracks/canada_flag.png', true) }}" alt="canada-flag">
+                <img class="schedule-track" src="{{ asset('resources/main/img/tracks/canada1.png', true) }}" alt="canada-track">
+                <div class="schedule-box">
+                    <div class="schedule-title">Data wyścigu</div>
+                    <div class="schedule-data">21 marca 2021</div>
+                </div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Nazwa toru</div>
+                    <div class="schedule-data">Circuit Gilles-Villeneuve</div>
+                </div>
+                <div class="schedule-header">Dane wyścigu</div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Długość okrążenia</div>
+                    <div class="schedule-data">4.361 km</div>
+                </div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Ilość okrążeń</div>
+                    <div class="schedule-data">35</div>
+                </div>
+                <div class="schedule-box">
+                    <div class="schedule-title">Długość wyścigu</div>
+                    <div class="schedule-data">152.635 km</div> 
+                </div>
             </div>
-            <div class="col-sm col-md-6 col-lg-4">
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
                 <p class="schedule-name">Wielka Brytania</p>
             </div>
-            <div class="col-sm col-md-6 col-lg-4">
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
                 <p class="schedule-name">Węgry</p>
             </div>
-            <div class="col-sm col-md-6 col-lg-4">
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
                 <p class="schedule-name">Belgia</p>
             </div>
-            <div class="col-sm col-md-6 col-lg-4">
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
                 <p class="schedule-name">Holandia</p>
             </div>
-            <div class="col-sm col-md-6 col-lg-4">
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
                 <p class="schedule-name">Włochy</p>
             </div>
-            <div class="col-sm col-md-6 col-lg-4">
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
                 <p class="schedule-name">Brazylia</p>
             </div>
         </div>
