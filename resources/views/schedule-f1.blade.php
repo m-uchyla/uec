f667480fa4668cacc04bb1d3d7c0475f9cf62d5a
Jakub Jankowski <fenus021@gmail.com>
2021-01-29 02:10:39 +0100

    pierwsza proba

--- resources/views/schedule-f1.blade.php
+++ resources/views/schedule-f1.blade.php
@@ -7,41 +7,44 @@
     <h1 class="name-page">Terminarz</h1>
     <div class="container">
         <div class="row">
-            <div class="col-sm col-md-6 col-lg-4 term-tor">
-            <p class="schedule-name">Bahrian</p>
+            <div class="col-sm col-md-6 col-lg-4 schedule-tor">
+                <p class="schedule-name">Bahrian</p>
+                <img class="schedule-track-flag" src="{{ asset('resources/main/img/tracks/bahrain_flag.png', true) }}" alt="bahrain-flag">
+                <img class="schedule-track" src="{{ asset('resources/main/img/tracks/bahrain1.png', true) }}" alt="bahrain-track">
+                <div class="schedule-track-date">3 marca 2021</div>
             </div>
             <div class="col-sm col-md-6 col-lg-4 term-tor">
-            <p class="schedule-name">Wietnam</p>
+                <p class="schedule-name">Wietnam</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Azerbejdżan</p>
+                <p class="schedule-name">Azerbejdżan</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Monako</p>
+                <p class="schedule-name">Monako</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Kanada</p>
+                <p class="schedule-name">Kanada</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Austria</p>
+                <p class="schedule-name">Austria</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Wielka Brytania</p>
+                <p class="schedule-name">Wielka Brytania</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Węgry</p>
+                <p class="schedule-name">Węgry</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Belgia</p>
+                <p class="schedule-name">Belgia</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Holandia</p>
+                <p class="schedule-name">Holandia</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Włochy</p>
+                <p class="schedule-name">Włochy</p>
             </div>
             <div class="col-sm col-md-6 col-lg-4">
-            <p class="schedule-name">Brazylia</p>
+                <p class="schedule-name">Brazylia</p>
             </div>
         </div>
     </div>
