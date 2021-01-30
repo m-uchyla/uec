ef77fa964a5305fedffcc1b51f8000d3ddb3762c
Jakub Milewski <kubim2002@gmail.com>
2021-01-30 03:41:16 +0100

    halo wyjeb

--- resources/views/regulamin-f1.blade.php
+++ resources/views/regulamin-f1.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.f1')
 
 @section('content')
+        <!--? Blog Area Start-->
+        <section class="about-area section-padding2"  id="terms" style="padding-top:0px">
+            <a href="#terms" class="btn" id="click" style="visibility:hidden"></a>
             <!--? Team Start -->
             <div class="team-area section-padding30" style="padding-top:50px">
                 <div class="container">
@@ -277,4 +280,32 @@
                 </div>
             </div>
         <section>
+@endsection
+
+@section('script')
+<script>
+
+$(document).ready(function(){
+    document.getElementById("click").click();
+});
+
+
+$('#nav').onePageNav({
+	currentClass: 'current',
+	changeHash: false,
+	scrollSpeed: 750,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: 'swing',
+	begin: function() {
+		//I get fired when the animation is starting
+	},
+	end: function() {
+		//I get fired when the animation is ending
+	},
+	scrollChange: function($currentListItem) {
+		//I get fired when you enter a section and I pass the list item of the section
+	}
+});
+</script>
 @endsection
