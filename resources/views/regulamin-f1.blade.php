a2c42d5e72c0d77e8458a75f768861d3b59a45dd
Jakub Milewski <kubim2002@gmail.com>
2021-01-30 03:35:24 +0100

    fix regulamin

--- resources/views/regulamin-f1.blade.php
+++ resources/views/regulamin-f1.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.f1')
 
 @section('content')
+            <!--? Team Start -->
+            <div class="team-area section-padding30" style="padding-top:50px">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-xl-6 col-lg-7 col-md-10 col-sm-10">
                             <!-- Section Tittle -->
                             <div class="section-tittle text-center mb-70">
                                 <span>Regulamin</span>
@@ -274,4 +279,30 @@
         <section>
 @endsection
 
+@section('script')
+<script>
 
+$(document).ready(function(){
+    document.getElementById("click").click();
+});
+
+
+$('#nav').onePageNav({
+	currentClass: 'current',
+	changeHash: false,
+	scrollSpeed: 750,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: 'swing',
+	begin: function() {
+		//I get fired when the animation is starting
+	},
+	end: function() {
+		//I get fired when the animation is ending
+	},
+	scrollChange: function($currentListItem) {
+		//I get fired when you enter a section and I pass the list item of the section
+	}
+});
+</script>
+@endsection
