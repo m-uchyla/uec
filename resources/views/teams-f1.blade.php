fa3761f3bffec6c5547db9ceb50cd702020a24e4
Jakub Milewski <kubim2002@gmail.com>
2021-02-09 05:23:25 +0100

    chuj w to

--- resources/views/teams-f1.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.f1')
-@section('content')
-<div class="background-f1">
-<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf6M7NWBfyU44Clg7tQsQ73ofy6iAhnr8bdGuqJ5w67UQTFzQ/viewform?embedded=true" width="640" height="870" frameborder="0" marginheight="0" marginwidth="0">Ładuję…</iframe>
-</div>
-@endsection
