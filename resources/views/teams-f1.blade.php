52ad8a3222a3919be37469f7039ac413f52ac94f
Jakub Milewski <kubim2002@gmail.com>
2021-02-09 05:06:17 +0100

    ss

---
+++ resources/views/teams-f1.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.f1')
+@section('content')
+<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf6M7NWBfyU44Clg7tQsQ73ofy6iAhnr8bdGuqJ5w67UQTFzQ/viewform?embedded=true" width="640" height="870" frameborder="0" marginheight="0" marginwidth="0">Ładuję…</iframe>
+@endsection
