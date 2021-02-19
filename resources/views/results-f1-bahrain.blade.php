3f1ed1f1dd8f91e735732f70e6f9430a3ee42472
Jakub Jankowski <fenus021@gmail.com>
2021-02-19 21:59:00 +0100

    xdxd1

--- resources/views/results-f1-bahrain.blade.php
+++ resources/views/results-f1-bahrain.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.f1')
 @section('content')
 <head>
-<title>Kwalifikacja Kierowców | UEC F1</title>
+<title>Wyniki - Bahrajn | UEC F1</title>
 </head>
 <div class="f1-background pb-5" >
     <h1 class="name-page">Wyniki - Bahrajn</h1>
