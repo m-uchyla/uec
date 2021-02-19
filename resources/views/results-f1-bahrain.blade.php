f5183053b410a963e5ea159965ba31768a7549b4
Jakub Jankowski <fenus021@gmail.com>
2021-02-19 21:48:27 +0100

    xdxd

--- resources/views/results-f1-bahrain.blade.php
+++ resources/views/results-f1-bahrain.blade.php
@@ -8,7 +8,7 @@
     <div class="margin-results">
         <table class="table table-striped table-dark table-f1 table-f1-results">
         <thead>
-            <tr id="head-border">
+            <tr id="head-border-results">
             <th scope="col" id="no-border" class="table-results-place">#</th>
             <th scope="col" id="no-border" class="table-results-name">Nick</th>
             <th scope="col" id="no-border" class="table-results-team">Zespół</th>
